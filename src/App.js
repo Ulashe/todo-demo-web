31174dee3296178f46325a2b63f8ada71b5aeabb
Ulas <ozulashe@gmail.com>
2021-11-14 16:12:46 +0300

    Styled components, styled system installed. Dynamic theme support added.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import { store, persistor } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import axios from "axios";
+import { ThemeProvider } from "styled-components";
 
 axios.defaults.baseURL = "http://192.168.1.104:3000/api";
 
 export default function App() {
+  const theme = store.getState().theme;
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}></PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id totam nisi dolor praesentium
+            ullam distinctio corrupti debitis. In, rerum mollitia! Adipisci excepturi dicta soluta
+            veritatis odio voluptatum cupiditate, magni aperiam!
+          </div>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
+import theme from "./theme";
 
-export default combineReducers({ auth });
+export default combineReducers({ auth, theme });
