172a028528b893dc1550fe8ec743ec6e72f8ecaa
Ulas <ozulashe@gmail.com>
2021-12-10 10:23:30 +0300

    React router added.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import axios from "axios";
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/layout";
+import TodoLists from "./components/todoLists";
 
 axios.defaults.baseURL = "http://192.168.1.104:3000/api";
 
@@ -13,11 +16,13 @@ export default function App() {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ThemeProvider theme={theme}>
-          <div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id totam nisi dolor praesentium
-            ullam distinctio corrupti debitis. In, rerum mollitia! Adipisci excepturi dicta soluta
-            veritatis odio voluptatum cupiditate, magni aperiam!
-          </div>
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<TodoLists />} />
+              </Routes>
+            </Layout>
+          </BrowserRouter>
         </ThemeProvider>
       </PersistGate>
     </Provider>
