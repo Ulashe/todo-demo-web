56cfad13a48e61faa618090709e7a4357a536007
Ulas <ozulashe@gmail.com>
2022-01-13 16:13:07 +0300

    Added loading state to text button.

--- src/components/textButton.js
+++ src/components/textButton.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import {
   variant,
   compose,
@@ -10,11 +10,41 @@ import {
   position,
   border,
 } from "styled-system";
+import Loading from "react-loading";
 
-export const TextButton = styled("div")`
+export const TextButton = ({
+  loading = false,
+  loadingType = "bars",
+  loadingSize = 28,
+  ...props
+}) => {
+  const theme = useTheme();
+
+  return (
+    <StyledTextButton {...props} onClick={loading ? undefined : props.onClick}>
+      {loading ? (
+        <StyledLoading
+          type={loadingType}
+          color={props.variant == "contained" ? "white" : theme.colors.blue[1]}
+          width={loadingSize}
+          height={loadingSize}
+          className="loading"
+        />
+      ) : (
+        props.children
+      )}
+    </StyledTextButton>
+  );
+};
+
+const StyledTextButton = styled("div")`
   text-align: center;
   cursor: pointer;
   border-style: solid;
   ${variant({ scale: "textButton" })}
   ${compose(layout, color, space, flexbox, position, border, typography)}
 `;
+
+const StyledLoading = styled(Loading)({
+  margin: "-5px 0",
+});
