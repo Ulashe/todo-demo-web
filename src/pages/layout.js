8d4bc94602be117d2ff36b0c1823fee2090cbf39
Ulas <ozulashe@gmail.com>
2022-01-19 11:14:24 +0300

    Added app loading screen.

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -3,11 +3,15 @@ import { Navbar } from "../components";
 import { useDispatch, useSelector } from "react-redux";
 import { getAuth, refresh, signOutThunk } from "../redux/reducers/authentication";
 import axios from "axios";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
+import { FlexBox, Text } from "../components/styled-components";
+import Loading from "react-loading";
+import { hslaAdjust } from "../utils/hslaAdjust";
 
 export default function Layout({ children }) {
   const auth = useSelector(getAuth);
   const dispatch = useDispatch();
+  const theme = useTheme();
   const [loading, setLoading] = React.useState(!!auth.refreshToken);
 
   React.useEffect(() => {
@@ -43,7 +47,18 @@ export default function Layout({ children }) {
   }, [auth.refreshToken, auth.expireDate]);
 
   return loading ? (
-    <div>placeholder</div>
+    <FlexBox
+      center
+      height="100vh"
+      vertical
+      gridRowGap={10}
+      bg={hslaAdjust({ color: theme.colors.primary, lAbs: 90, sAbs: 20 })}
+    >
+      <Text color="primary" fontSize={40} fontWeight="bold">
+        Todo Lists App
+      </Text>
+      <Loading type="bars" color={theme.colors.primary} />
+    </FlexBox>
   ) : (
     <div>
       <Navbar />
